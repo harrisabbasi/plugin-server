3b76c7b6bf530cb44d9e79f867a61dfc6ea10a68
Harris Abbasi <harrisabbasi01@hotmail.com>
2022-12-16 23:38:56 +0500

    Stripe Card Details Registration and Update

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 require_once('Medoo-master/src/Medoo.php');
+require 'vendor/autoload.php';
 use Medoo\Medoo;
 
 if (isset($_GET['eco_bag_token']) & isset($_GET['return_url'])){
@@ -8,13 +9,14 @@ if (isset($_GET['eco_bag_token']) & isset($_GET['return_url'])){
     // [required]
     'type' => 'mysql',
     'host' => 'localhost',
-    'database' => 'eco_bag',
+    'database' => 'eco_bag_stripe',
     'username' => 'root',
     'password' => '',
   ]);
 
   $data = $database->select("tokens_list", [
-      "eco_bag_token"
+      "eco_bag_token",
+      "company"
     ], [
       "eco_bag_token" => $_GET['eco_bag_token']
     ]);
@@ -28,12 +30,47 @@ if (isset($_GET['eco_bag_token']) & isset($_GET['return_url'])){
 } else {
  echo 'Something went wrong. Please try again';
  exit; 
-} ?>
+}
+
+$stripe = new \Stripe\StripeClient(
+  'sk_test_51MFcSvBsDABuUoiXtoHX3oYHzXjyj9U6W1lQTUY1q0wMRGQ6EBKvQzHpXh82XAiftyEqV0395SXAeZzY0aUrTqrT00wH85eCL4'
+);
+
+if (isset($_GET['action']) && $_GET['action'] == 'update'){
+  $row = $database->select("tokens_db", 
+    "*",
+    ["eco_bag_token" => $_GET['eco_bag_token']]
+  );
+
+  $customer = $stripe->customers->retrieve(
+    $row[0]['payment_token'],
+    []
+  );
+
+}
+else{
+  $customer = $stripe->customers->create([
+  'name' => $data[0]['company'],
+]);
+
+
+}
+
+$intent = $stripe->setupIntents->create(
+  [
+    'customer' => $customer->id,
+    'payment_method_types' => ['bancontact', 'card', 'ideal'],
+  ]
+);
+
+$client_secret = $intent->client_secret;
+?>
 <head>
   <meta charset="utf-8">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@0;1&display=swap" rel="stylesheet">
+  <script src="https://js.stripe.com/v3/"></script>
   <style>
     body{
       padding:20px;
@@ -61,71 +98,66 @@ if (isset($_GET['eco_bag_token']) & isset($_GET['return_url'])){
   </style>
 </head>
 <body>
-  <!-- Step one: add an empty container to your page -->
-  <form id="payment-form" action="server.php" method="post">
-      <!-- Putting the empty container you plan to pass to
-        `braintree.dropin.create` inside a form will make layout and flow
-        easier to manage -->
-    <div id="dropin-container"></div>
-    <h2>Enter your credentials:</h2>
-    <input class="input" type="" name="first_name" placeholder="First Name" required>
-    <input class="input" type="" name="last_name" placeholder="Last Name" required>
-    <input class="input" type="" name="company" placeholder="Company" required>
-    <input type="submit" />
-
-    <input type="hidden" id="nonce" name="payment_method_nonce"/>
-    <input type="hidden"  name="eco_bag_token" value="<?php echo $_GET['eco_bag_token'] ?>"/>
-    <input type="hidden"  name="return_url" value="<?php echo $_GET['return_url'] ?>"/>
-    <?php if (isset($_GET['action']) && $_GET['action'] === 'update'){?>
-      <input type="hidden"  name="pick_pack_update" value="update"/>
-    <?php } ?>
+  <form id="payment-form">
+    <div id="payment-element">
+      <!-- Elements will create form elements here -->
+    </div>
+    <input type="submit" id="submit"></button>
+    <div id="error-message">
+      <!-- Display error message to your customers here -->
+    </div>
   </form>
 
   <script
   src="https://code.jquery.com/jquery-3.6.1.min.js"
   integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ="
   crossorigin="anonymous"></script>
-  <script src="https://js.braintreegateway.com/web/dropin/1.33.4/js/dropin.min.js"></script>
-
-  <script type="text/javascript">
-  // call `braintree.dropin.create` code here
-  var token;
-  const form = document.getElementById('payment-form');
-
-  $(document).ready(function() {
-   // Step One ajax request to our server that returns client token
-   $.post("server.php",
-   {
-     process: "token"
-   },
-   function(data, status){
-     console.log("Data: " + data + "\nStatus: " + status);
-     token = data;
-     // Request to braintree server with client token (and the dropin container is created)
-     braintree.dropin.create({
-       container: document.getElementById('dropin-container'),
-       authorization: token,
-       // ...plus remaining configuration
-     }, (error, dropinInstance) => {
-       // Use `dropinInstance` here
-       // Methods documented at https://braintree.github.io/braintree-web-drop-in/docs/current/Dropin.html
-       form.addEventListener('submit', event => {
-          event.preventDefault();
-
-          dropinInstance.requestPaymentMethod((error, payload) => {
-            if (error) console.error(error);
-
-            
-            document.getElementById('nonce').value = payload.nonce;
-            form.submit();
-          });
-        });
-     });
-     
-   });
- });
   
 
+  <script type="text/javascript">
+  
+
+  const stripe = Stripe('pk_test_51MFcSvBsDABuUoiXnPU6GqCf3AaoQqyQ1xH1Xk4MG3Z8XDQNlsiYlbBHzwPHdrs3y9kBeOnJEhcQyK0CDDLagS3X00MY2Wbjuu');
+  
+  const options = {
+    clientSecret: '<?php echo $client_secret ?>',
+    // Fully customizable with appearance API.
+    appearance: {/*...*/},
+  };
+
+  // Set up Stripe.js and Elements to use in checkout form, passing the client secret obtained in step 3
+  const elements = stripe.elements(options);
+
+  // Create and mount the Payment Element
+  const paymentElement = elements.create('payment');
+  paymentElement.mount('#payment-element');
+
+  const form = document.getElementById('payment-form');
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const {error} = await stripe.confirmSetup({
+      //`Elements` instance that was used to create the Payment Element
+      elements,
+      confirmParams: {
+        return_url: 'http://localhost/plugin_server/return_url.php?eco_bag_token=' + '<?php echo $_GET['eco_bag_token'] ?>' + '&customer=' + '<?php echo $customer->id ?>' + '&intent=' + '<?php echo $intent->id?>',
+      }
+    });
+
+    if (error) {
+      // This point will only be reached if there is an immediate error when
+      // confirming the payment. Show error to your customer (for example, payment
+      // details incomplete)
+      const messageContainer = document.querySelector('#error-message');
+      messageContainer.textContent = error.message;
+    } else {
+      // Your customer will be redirected to your `return_url`. For some payment
+      // methods like iDEAL, your customer will be redirected to an intermediate
+      // site first to authorize the payment, then redirected to the `return_url`.
+    }
+  });
+  
   
   
   </script>
---
+++ return_url.php
@@ -0,0 +1,80 @@
+<?php
+require_once('Medoo-master/src/Medoo.php');
+require 'vendor/autoload.php';
+use Medoo\Medoo;
+
+if (isset($_GET['eco_bag_token']) & isset($_GET['customer'])){
+
+  $database = new Medoo([
+    // [required]
+    'type' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'eco_bag_stripe',
+    'username' => 'root',
+    'password' => '',
+  ]);
+
+  $data = $database->select("tokens_list", [
+      "eco_bag_token",
+      "company"
+    ], [
+      "eco_bag_token" => $_GET['eco_bag_token']
+    ]);
+
+  if ($database->error || count($data) != 1){
+    echo 'There was some error retrieving the token';
+    exit;
+  }
+
+
+} else {
+ echo 'Something went wrong. Please try again';
+ exit; 
+}
+
+$database->create("tokens_db", [
+	"id" => [
+		"INT",
+		"NOT NULL",
+		"AUTO_INCREMENT",
+		"PRIMARY KEY"
+	],
+	"eco_bag_token" => [
+		"VARCHAR(32)",
+		"NOT NULL"
+	],
+	"payment_token" => [
+		"VARCHAR(32)",
+		"NOT NULL"
+	]
+]);
+
+$stripe = new \Stripe\StripeClient(
+  'sk_test_51MFcSvBsDABuUoiXtoHX3oYHzXjyj9U6W1lQTUY1q0wMRGQ6EBKvQzHpXh82XAiftyEqV0395SXAeZzY0aUrTqrT00wH85eCL4'
+);
+$intent = $stripe->setupIntents->retrieve(
+  $_GET['intent'],
+  []
+);
+
+if ($intent->status == 'succeeded'){
+
+	$row = $database->select("tokens_db", 
+		"*",
+		["eco_bag_token" => $_GET['eco_bag_token']]
+	);
+
+	if (empty($row)){
+		$database->insert("tokens_db", [
+			"eco_bag_token" => $_GET['eco_bag_token'],
+			"payment_token" => $_GET['customer'],
+		]);
+	}
+	//make it default source using the customer object, you can retreive the default source (payment method id) using the intent object
+}
+
+
+
+
+?>
+
